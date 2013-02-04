efa11531613a54ad146e66ab67125e5e56f31541
Krum Bakalsky <krum.bakalsky@sap.com>
2013-02-04 13:16:25 +0200

    Update instructions with specifics for AR-JDBC-Adapter 1.2.6
    Signed-off-by: Krum Bakalsky <krum.bakalsky@sap.com>

--- photoapp/config/environment.rb
+++ photoapp/config/environment.rb
@@ -1,8 +1,10 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-# Needed for the local case for asset precompilation.
+# Needed for the local case for asset precompilation, db migration, etc.
 # Please comment these two lines out when deploying on NetWeaver Cloud
+# Note: This is fixed and no longer required since AR-JDBC Adapter v. 1.2.6
+# 
 # require 'jdbc/sqlite3'
 # Jdbc::SQLite3.load_driver(:require) if Jdbc::SQLite3.respond_to?(:load_driver)
 
